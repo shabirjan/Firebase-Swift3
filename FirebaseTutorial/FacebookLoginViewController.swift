663a5eb3ab06bd28095b80c66dcef6ce349526fc
Shabir Jan <iam.shabir@yahoo.com>
2017-02-05 15:52:59 +0500

    Added Controller for Facebook login

---
+++ FirebaseTutorial/FacebookLoginViewController.swift
@@ -0,0 +1,37 @@
+//
+//  FacebookLoginViewController.swift
+//  FirebaseTutorial
+//
+//  Created by Shabir Jan on 05/02/2017.
+//  Copyright © 2017 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class FacebookLoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func btnSignInFb(_ sender: Any) {
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

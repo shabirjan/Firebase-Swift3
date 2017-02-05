22331580a404ed6d739f3c446c24aa2886b20411
Shabir Jan <iam.shabir@yahoo.com>
2017-02-05 15:09:56 +0500

    Implemented Logout Functionality

--- FirebaseTutorial/HomeViewController.swift
+++ FirebaseTutorial/HomeViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import Firebase
 
 class HomeViewController: UIViewController {
 
@@ -24,6 +24,14 @@ class HomeViewController: UIViewController {
     
 
     @IBAction func logOutAction(sender: AnyObject) {
-        
+        if FIRAuth.auth()?.currentUser != nil {
+            do{
+                try FIRAuth.auth()?.signOut()
+                let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "SignUp")
+                present(vc, animated: true, completion: nil)
+            }catch let error as NSError{
+                print(error.localizedDescription)
+            }
+        }
     }
 }

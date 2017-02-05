6649466403513b2500e212623a97d1ec4fc09dfb
Shabir Jan <iam.shabir@yahoo.com>
2017-02-05 16:29:27 +0500

    Added Fblogin functionality and sign out as well. Added check to see if user is already logged using Fb and Firebase

--- FirebaseTutorial/FacebookLoginViewController.swift
+++ FirebaseTutorial/FacebookLoginViewController.swift
@@ -7,31 +7,74 @@
 //
 
 import UIKit
-
+import Firebase
+import FBSDKLoginKit
 class FacebookLoginViewController: UIViewController {
-
+    
+    @IBOutlet weak var btnFb: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     @IBAction func btnSignInFb(_ sender: Any) {
+        if FIRAuth.auth()?.currentUser == nil{
+            let fbLoginManager = FBSDKLoginManager()
+            if FBSDKAccessToken.current() == nil
+            {
+                fbLoginManager.logIn(withReadPermissions: ["public_profile","email"], from: self) { (result, error) in
+                    if let error = error{
+                        print(error.localizedDescription)
+                        return
+                    }
+                }
+            }
+            
+            guard let accessToken = FBSDKAccessToken.current() else{
+                print("Failed to get access token")
+                return
+            }
+            
+            let creds = FIRFacebookAuthProvider.credential(withAccessToken: accessToken.tokenString)
+            
+            //Calling Login by using Firebase Api
+            FIRAuth.auth()?.signIn(with: creds, completion: { [weak self](user, error) in
+                if error != nil {
+                    print("Login error: \(error?.localizedDescription)")
+                    let alertController = UIAlertController(title: "Login Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    let okayAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(okayAction)
+                    self?.present(alertController, animated: true, completion: nil)
+                    
+                    return
+                }else{
+                    self?.btnFb.setTitle("Sign out", for:.normal)
+                    if let currentUser = FIRAuth.auth()?.currentUser
+                    {
+                        let alertController = UIAlertController(title: "Login Success", message: "Welcome \(currentUser.displayName!)", preferredStyle: .alert)
+                        let okayAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                        alertController.addAction(okayAction)
+                        self?.present(alertController, animated: true, completion: nil)
+                    }
+                }
+            })
+        }else{
+            do{
+                try FIRAuth.auth()?.signOut()
+            }catch let error as NSError{
+                print(error.localizedDescription)
+            }
+        }
+        
+        
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+    
+    
 }

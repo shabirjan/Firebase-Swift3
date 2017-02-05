c5a88a6c185f7862d8173201313a014a4cde45d7
Shabir Jan <iam.shabir@yahoo.com>
2017-02-05 15:14:48 +0500

    Implemented Password Reset Functionality

--- FirebaseTutorial/ResetPasswordViewController.swift
+++ FirebaseTutorial/ResetPasswordViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import Firebase
 
 class ResetPasswordViewController: UIViewController {
     
@@ -17,6 +17,35 @@ class ResetPasswordViewController: UIViewController {
     // Reset Password Action
     @IBAction func submitAction(_ sender: AnyObject)
     {
+        if  self.emailTextField.text! == "" {
+            let alertController = UIAlertController(title: "Oops!", message: "Please enter an email.", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            present(alertController, animated: true, completion: nil)
+        }else{
+            FIRAuth.auth()?.sendPasswordReset(withEmail: emailTextField.text!, completion: { (error) in
+                var title = ""
+                var message = ""
+                
+                if error != nil {
+                    title = "Error!"
+                    message = (error?.localizedDescription)!
+                } else {
+                    title = "Success!"
+                    message = "Password reset email sent."
+                    self.emailTextField.text = ""
+                }
+                
+                let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+                
+                let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                alertController.addAction(defaultAction)
+                
+                self.present(alertController, animated: true, completion: nil)
+            })
+        }
     }
     
 
